499a34e7915d2f045e250918a1588e69068ab55c
Kósa Áron <90419831+ar0nK@users.noreply.github.com>
2025-03-30 13:53:54 +0200

    Update server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const ngrok = require('ngrok');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.send('a szerver elindult');
+});
+
+
+app.listen(PORT, async () => {
+    console.log(`A szerver cím: http://localhost:${PORT}`);
+
+    try {
+        
+        const url = await ngrok.connect(PORT);
+        console.log(`ngrok url: ${url}`);
+    } catch (err) {
+        console.error('Hiba az ngrok indításakor:', err);
+    }
+});
