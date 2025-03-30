19593cca1beffd000ccf89a9da9e2c103117ebc7
Kósa Áron <90419831+ar0nK@users.noreply.github.com>
2025-03-30 14:29:32 +0200

    alap szerver
    betölti az oldalt

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const ngrok = require('ngrok');
 
 app.use(cors());
 app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'main.html'));
+});
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
