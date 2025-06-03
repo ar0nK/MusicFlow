ce2d400d0337de99f72b352de751edd74cd4cba4
Kósa Áron <90419831+ar0nK@users.noreply.github.com>
2025-06-03 18:57:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const ngrok = require('ngrok');
 app.use(cors());
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'test.html'));
+    res.sendFile(path.join(__dirname, 'public', 'main.html'));
 });
 app.use(express.static(path.join(__dirname, 'public')));
 
